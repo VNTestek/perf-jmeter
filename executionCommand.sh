f63cd7b5d12b2e9311b7c37184c27f19535402a9
Vincent <duy.bg@shb.com.vn>
2026-02-24 00:11:09 +0700

    Vincent: Update Elearning

---
+++ executionCommand.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Kiểm tra nếu không có đủ tham số được truyền vào
+if [ "$#" -lt 5 ]; then
+  echo "Sử dụng: $0 <Test Scenario Name [Type]_[Product], Ex: Load_SAHA> <DateRes> <CCU> <Rampup> <Step> <Duration>"
+  echo "Example: ./execution.sh 20241016_Transfer_Napas SAHA_Napas_Load 20241224 1 1 1 1"
+  exit 1
+fi
+
+# Configuration
+jmx=$1
+scenario=$2
+resFol=$3
+ccu=$4
+ramp=$5
+steps=$6
+duration=$7
+opt=$8
+influxToken=$9
+influxOrg=${10}
+influxBucket=${11}
+influxUrl=${12}
+
+currentDate=$(date +'%Y%m%d_%H%M%S')
+if [ -z "$worker" ] || [ "$worker" = "null" ]; then
+    # Trường hợp worker rỗng
+    resPath="${currentDate}_${scenario}_${ccu}C${ramp}R${steps}S${duration}D"
+else
+    # Trường hợp worker có giá trị
+    resPath="${currentDate}_${scenario}_${ccu}C${ramp}R${steps}S${duration}D_${worker}"
+fi
+
+if [ -z "$opt" ] || [ "$opt" = "OPT" ]; then
+    opt =""
+fi
+
+#resPath=${currentDate}_${scenario}_${ccu}C${ramp}R${steps}S${duration}D
+reportPath="res/${resFol}/${resPath}"
+echo "=================================================================================="
+echo " Test Scenario:  ${scenario}    Execution Time:  ${currentDate}"
+echo " Test Info: CCU:  ${ccu}  Ramp-up:  ${ramp}m   Step: ${steps}  Duration:  ${duration}m    Opt: ${opt}"
+echo " Report Path: ${reportPath}"
+echo "=================================================================================="
+
+mkdir -p ${reportPath}
+sleep 5
+
+echo "./bin/jmeter  -Jjmeter.save.saveservice.output_format=csv -Jjmeter.save.saveservice.response_data=true -Jjmeter.save.saveservice.samplerData=true -Jjmeter.save.saveservice.requestHeaders=true -Jjmeter.save.saveservice.responseHeaders=true -Jjmeter.save.saveservice.url=true -n -t jmx/${jmx}.jmx -l ${reportPath}/${resPath}.jtl -DRPath=${reportPath}/${resPath}.xml -DUSERS=${ccu} -DRAMP=${ramp} -DSTEP=${steps} -DDURA=${duration} -DORG=${influxOrg} -DBUCKET=${influxBucket} -DTOKEN=${influxToken} -DDBMETRIC=${influxUrl}"
+./bin/jmeter  -Jjmeter.save.saveservice.output_format=csv -Jjmeter.save.saveservice.response_data=true -Jjmeter.save.saveservice.samplerData=true -Jjmeter.save.saveservice.requestHeaders=true -Jjmeter.save.saveservice.responseHeaders=true -Jjmeter.save.saveservice.url=true -n -t jmx/${jmx}.jmx -l ${reportPath}/${resPath}.jtl -DRPath=${reportPath}/${resPath}.xml ${opt} -DORG=${influxOrg} -DBUCKET=${influxBucket} -DTOKEN=${influxToken} -DDBMETRIC=${influxUrl}
+
+#./bin/jmeter  -Jjmeter.save.saveservice.output_format=csv -Jjmeter.save.saveservice.response_data=true -Jjmeter.save.saveservice.samplerData=true -Jjmeter.save.saveservice.requestHeaders=true -Jjmeter.save.saveservice.responseHeaders=true -Jjmeter.save.saveservice.url=true -n -t jmx/20241021_ESB_Card.jmx -l res/${resPath}/result.jtl -DUSERS=${ccu} -DRAMP=${ramp} -DSTEP=${steps} -DDURA=${duration} -DORG=Automation_Test -DBUCKET=SAHA -DRUNID=POD1_1024_ESB
+#-e -o res/${resPath}
+sleep 10
+
+echo "===== Generate HTML Report ====="
+./bin/jmeter -g res/${resFol}/${resPath}/${resPath}.jtl -o res/${resFol}/${resPath}/html
+echo "=====================END OF TESTING ==============================================="
+sleep 30
+
+sleep 10
+#./bin/jmeter -Jjmeter.save.saveservice.output_format=xml -Jjmeter.save.saveservice.response_data=true -Jjmeter.save.saveservice.samplerData=true -Jjmeter.save.saveservice.requestHeaders=true -J jmeter.save.saveservice.responseHeaders=true -Jjmeter.save.saveservice.url=true -n -t jmx/20241021_ESB_Transfer.jmx -l res/${resPath}/result.jtl  -e -o res/${resPath} -DUSERS=${ccu} -DRAMP=${ramp} -DSTEP=${steps} -DDURA=${duration} -DORG=Automation_Test -DBUCKET=SAHA -DRUNID=POD1_1024_ESB
